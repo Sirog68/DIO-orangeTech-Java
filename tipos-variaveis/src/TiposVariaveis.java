4127b17fa3c54d3c45968076880a76611b22101e
Silvio Mancilha <srmancilha@gmail.com>
2023-01-05 18:43:04 -0300

    alteração readme e exemplos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int numero = 5;
+        numero = 10;
+        System.out.println(numero);
+
+
+        final double NUMERO_DE_PI = 3.14;
+        /* NUMERO_DE_PI = 17.15; --> variável como uma constante, utilizamos a palavra `final`, mas por convenção, 
+        esta variável deverá ser escrita toda em caixa alta. */
     }
 }
